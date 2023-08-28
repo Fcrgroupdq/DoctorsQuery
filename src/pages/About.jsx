58b8f5355e07eb5eba8178d34530abdd8e5a22aa
Fcrgroupdq <137594100+Fcrgroupdq@users.noreply.github.com>
2023-08-28 15:49:08 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <center><h2>About</h2> </center>
+      <p>DOCTORSQUERIES is a brand new platform in the online global marketing world situated in Dwarka. This is foremost, the most modern marketing strategy that has been developed by the developers by fore-seeing the well-being and efficient medical accessibility for billions and billions of National and International patients around the globe.</p>
+      <br>
+      <p>The DOCTORSQUERIES will set an exquisite example in the medical industry, by reaching out to the customers to get accurate and precise information regarding the most required HEALTHCARE SECTOR in a budget friendly way.</p>
+    <br>
+      DOCTORSQUERIES always strive forward believing the famous ideology “If wealth is lost nothing is lost but, if health is lost everything is lost”. Life is a beautiful journey that is gifted by the GOD himself to be enjoyed with utmost happiness and prosperity till the very end thus, we are fulfilling this wish to be affordable to every single customer.
+    
+    </div>
+    
+    
+    
   )
 }
 
